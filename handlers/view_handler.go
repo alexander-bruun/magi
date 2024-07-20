c20438e9ad05db34b8a6f098016c4548f8ab470f
Alexander Bruun <alexander.bruun@gmail.com>
2024-07-20 09:31:19 +0200

    Added mangas 3x3 page grid

--- handlers/view_handler.go
+++ handlers/view_handler.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"strconv"
+
 	"github.com/a-h/templ"
 	"github.com/alexander-bruun/magi/models"
 	"github.com/alexander-bruun/magi/views"
@@ -24,7 +26,17 @@ func HandleHome(c *fiber.Ctx) error {
 }
 
 func HandleMangas(c *fiber.Ctx) error {
-	return HandleView(c, views.Mangas())
+	page, err := strconv.Atoi("page")
+	if err != nil || page <= 0 {
+		page = 1
+	}
+
+	mangas, count, err := models.SearchMangas("", page, 9, "name", "asc", "", 0)
+	if err != nil {
+		return HandleView(c, views.Error(err.Error()))
+	}
+
+	return HandleView(c, views.Mangas(mangas, count))
 }
 
 func HandleManga(c *fiber.Ctx) error {
