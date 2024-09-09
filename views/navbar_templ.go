8f95623c345033b6ff1a8ec1da47392261b9e88a
Alexander Bruun <alexander.bruun@gmail.com>
2024-09-09 18:24:09 +0200

    fix: Removed image from stack and added forbidden for banned users

--- views/navbar_templ.go
+++ views/navbar_templ.go
@@ -185,7 +185,7 @@ func OneDoesNotSimplySearch() templ.Component {
 			templ_7745c5c3_Var7 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<img class=\"uk-align-center mb-0\" width=\"200px\" src=\"/assets/img/one-does-not-simply.jpg\"><p class=\"italic uk-text-center\">One does not simply search...</p>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<p class=\"italic uk-text-center\">One does not simply search...</p>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
