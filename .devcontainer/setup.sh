0b1f4c87cf5949b58bedc00e3f3bb8944a5043f1
Alexander Bruun <87643857+alexander-bruun@users.noreply.github.com>
2024-10-30 19:02:09 +0000

    Updated devcontainer conf

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,42 +1,13 @@
 # install curl, git, ...
 dnf update -y
-dnf install -y curl git jq
+dnf install -y curl git jq go
 
 useradd -m user
 su user
 
-# install go
-VERSION='1.23.2'
-OS='linux'
-ARCH='amd64'
-
-curl -OL https://dl.google.com/go/go${VERSION}.${OS}-${ARCH}.tar.gz
-tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
-rm go$VERSION.$OS-$ARCH.tar.gz
-
 INSTALLED_GO_VERSION=$(go version)
 echo "Go version ${INSTALLED_GO_VERSION} is installed"
 
-# install gopls, dlv, hey
-echo "Getting development tools"
-go get -u golang.org/x/tools/gopls
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/rakyll/hey
-
-# vscode-go dependencies 
-echo "Getting dependencies for the vscode-go plugin "
-# via: https://github.com/microsoft/vscode-go/blob/master/.travis.yml
-go get -u -v github.com/acroca/go-symbols
-go get -u -v github.com/cweill/gotests/...
-go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-go get -u -v github.com/haya14busa/goplay/cmd/goplay
-go get -u -v github.com/mdempsky/gocode
-go get -u -v github.com/ramya-rao-a/go-outline
-go get -u -v github.com/rogpeppe/godef
-go get -u -v github.com/sqs/goreturns
-go get -u -v github.com/uudashr/gopkgs/cmd/gopkgs
-go get -u -v github.com/zmb3/gogetdoc
-go get -u -v golang.org/x/lint/golint
-go get -u -v golang.org/x/tools/cmd/gorename
-
+go install
+go install github.com/air-verse/air@latest
 go install github.com/a-h/templ/cmd/templ@latest
