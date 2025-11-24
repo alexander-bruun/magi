d119e05ddfb334349dc00cca56a8ff2e742688e1
Alexander Bruun <alexander.bruun@gmail.com>
2025-11-24 09:16:19 +0100

    fix: Add captcha to requests with high bot score

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.3
 
 require (
 	github.com/a-h/templ v0.3.960
+	github.com/chai2010/webp v1.4.0
 	github.com/gofiber/fiber/v2 v2.52.10
 	github.com/gofiber/template/html/v2 v2.1.3
 	github.com/gofiber/websocket/v2 v2.2.1
@@ -18,7 +19,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/chai2010/webp v1.4.0 // indirect
+	github.com/dchest/captcha v1.1.0 // indirect
 	github.com/fasthttp/websocket v1.5.3 // indirect
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
@@ -30,8 +31,6 @@ require (
 	github.com/ncruces/julianday v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
-	github.com/tdewolff/minify/v2 v2.24.7 // indirect
-	github.com/tdewolff/parse/v2 v2.8.5 // indirect
 	github.com/tetratelabs/wazero v1.10.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.59.0 // indirect
---
+++ handlers/captcha_handler.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"github.com/alexander-bruun/magi/views"
+	"github.com/dchest/captcha"
+	"github.com/gofiber/fiber/v2"
+)
+
+// HandleCaptchaPage serves the captcha verification page
+func HandleCaptchaPage(c *fiber.Ctx) error {
+	id := captcha.NewLen(6)
+	errorMsg := ""
+	if c.Query("error") == "invalid" {
+		errorMsg = "Invalid captcha. Please try again."
+	}
+
+	return HandleView(c, views.Captcha(errorMsg, id))
+}
+
+// HandleCaptchaImage serves captcha images
+func HandleCaptchaImage(c *fiber.Ctx) error {
+	c.Type("png")
+	captcha.WriteImage(c.Response().BodyWriter(), c.Params("id"), 240, 80)
+	return nil
+}
+
+// HandleCaptchaNew generates a new captcha ID
+func HandleCaptchaNew(c *fiber.Ctx) error {
+	id := captcha.NewLen(6)
+	return c.JSON(fiber.Map{"captcha_id": id})
+}
+
+// HandleCaptchaVerify verifies captcha answers
+func HandleCaptchaVerify(c *fiber.Ctx) error {
+	id := c.FormValue("id")
+	answer := c.FormValue("answer")
+	if captcha.VerifyString(id, answer) {
+		c.Cookie(&fiber.Cookie{
+			Name:     "captcha_solved",
+			Value:    "true",
+			MaxAge:   3600, // 1 hour
+			HTTPOnly: true,
+			Secure:   isSecureRequest(c),
+			SameSite: fiber.CookieSameSiteLaxMode,
+		})
+		// Redirect back to the original page
+		redirectURL := c.Cookies("captcha_redirect")
+		if redirectURL == "" {
+			redirectURL = "/"
+		}
+		return c.Redirect(redirectURL, fiber.StatusSeeOther)
+	}
+	return c.Redirect("/captcha?error=invalid", fiber.StatusSeeOther)
+}
